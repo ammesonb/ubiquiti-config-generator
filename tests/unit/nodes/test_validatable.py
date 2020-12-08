bceb6c3aa518390d510849824e65e530db38fc3c
Brett Ammeson <ammesonb@gmail.com>
2020-12-08 16:41:41 -0500

    Adds test for validatable object

---
+++ tests/unit/nodes/test_validatable.py
@@ -0,0 +1,55 @@
+"""
+Tests validatable object
+"""
+
+from ubiquiti_config_generator.nodes.validatable import Validatable
+
+
+# pylint: disable=protected-access
+
+
+def test_attributes_added():
+    """
+    .
+    """
+    obj = Validatable({"an-attribute": True}, ["an-attribute"])
+    assert obj._validate_attributes == ["an-attribute"], "Attributes set"
+    assert obj._validator_map == {"an-attribute": True}, "Map set"
+
+    obj._add_validate_attribute("other-attribute")
+    assert obj._validate_attributes == [
+        "an-attribute",
+        "other-attribute",
+    ], "Attributes added"
+    obj._add_keyword_attributes({"third-attr": 3, "fourth-attr": 4})
+    assert obj._validate_attributes == [
+        "an-attribute",
+        "other-attribute",
+        "third-attr",
+        "fourth-attr",
+    ], "Attributes updated"
+    assert getattr(obj, "third-attr") == 3, "Third attribute set"
+    assert getattr(obj, "fourth-attr") == 4, "Fourth attribute set"
+
+
+def test_validate(monkeypatch):
+    """
+    .
+    """
+    obj = Validatable(
+        {
+            "valid-attr": lambda value: True,
+            "invalid-attr": lambda value: False,
+            "even-attr": lambda value: value % 2 == 0,
+        }
+    )
+
+    assert obj.validate(), "No attributes is valid"
+    obj._add_keyword_attributes({"valid-attr": "something"})
+    assert obj.validate(), "Valid attr is valid"
+    obj._add_keyword_attributes({"even-attr": 1})
+    assert not obj.validate(), "Odd attr breaks validation"
+    setattr(obj, "even-attr", 2)
+    assert obj.validate(), "Even attr fixes validation"
+    obj._add_keyword_attributes({"invalid-attr": "value"})
+    assert not obj.validate(), "Invalid attr breaks validation"
