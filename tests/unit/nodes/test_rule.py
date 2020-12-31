c30de04ab334c164d54ff43005d062f9866111aa
Brett Ammeson <ammesonb@gmail.com>
2020-12-31 13:43:25 -0500

    Adds missed rule unit tests

---
+++ tests/unit/nodes/test_rule.py
@@ -0,0 +1,80 @@
+"""
+Test firewall rule
+"""
+
+from ubiquiti_config_generator.nodes import Rule
+
+
+def test_non_address_commands():
+    """
+    .
+    """
+    rule_properties = {
+        "action": "reject",
+        "description": "Its a firewall rule",
+        "log": "disable",
+        "protocol": "tcp",
+        "state": {
+            "new": "disable",
+            "invalid": "disable",
+            "related": "enable",
+            "established": "enable",
+        },
+    }
+    commands = Rule(10, "firewall1", **rule_properties).commands()
+
+    rule_base = "firewall name firewall1 rule 10 "
+    assert commands == [
+        rule_base + "action reject",
+        rule_base + "description 'Its a firewall rule'",
+        rule_base + "log disable",
+        rule_base + "protocol tcp",
+        rule_base + "state new disable",
+        rule_base + "state invalid disable",
+        rule_base + "state related enable",
+        rule_base + "state established enable",
+    ], "Rule commands correct"
+
+
+def test_non_group_address_commands():
+    """
+    .
+    """
+    rule_properties = {
+        "description": "rule",
+        "source": {"address": "10.10.10.10", "port": 8080},
+        "destination": {"address": "8.8.8.8", "port": "443"},
+    }
+    commands = Rule(10, "firewall1", **rule_properties).commands()
+    rule_base = "firewall name firewall1 rule 10 "
+
+    assert commands == [
+        rule_base + "action accept",
+        rule_base + 'description "rule"',
+        rule_base + "source address 10.10.10.10",
+        rule_base + "source port 8080",
+        rule_base + "destination address 8.8.8.8",
+        rule_base + "destination port 443",
+    ], "Rule commands correct"
+
+
+def test_group_address_commands():
+    """
+    .
+    """
+    rule_properties = {
+        "description": "rule",
+        "source": {"address": "a-group", "port": "web-ports"},
+        "destination": {"address": "the.whole.internet", "port": "any"},
+    }
+    commands = Rule(10, "firewall1", **rule_properties).commands()
+    rule_base = "firewall name firewall1 rule 10 "
+
+    assert commands == [
+        rule_base + "action accept",
+        rule_base + 'description "rule"',
+        rule_base + "source group address-group a-group",
+        rule_base + "source group port-group web-ports",
+        rule_base + "destination group address-group the.whole.internet",
+        rule_base + "destination group port-group any",
+    ], "Rule commands correct"
